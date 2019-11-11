5e754354ad7dc656ec7707db4d08f420ca5d657f
Laurent BERGE <laurent.berge@uni.lu>
2019-11-11 16:17:43 +0100

    cond means fun

--- src/parallel_funs.cpp
+++ src/parallel_funs.cpp
@@ -537,8 +537,82 @@ List cpppar_which_na_inf_mat(NumericMatrix mat, int nthreads){
 
 
 
+// [[Rcpp::export]]
+List cpppar_cond_means(NumericMatrix mat_vars, IntegerVector treat, int nthreads = 1){
+    // conditional means: function did_means
+
+    int N = mat_vars.nrow();
+    int K = mat_vars.ncol();
+
+    // objects to return:
+    IntegerVector na_vect(K);
+    NumericMatrix mean_mat(K, 2);
+    NumericMatrix sd_mat(K, 2);
+    IntegerMatrix n_mat(K, 2);
+    IntegerVector n_01(2);
 
 
+    // computation
+#pragma omp parallel for num_threads(nthreads)
+    for(int k=0 ; k<K ; ++k){
+
+        double sum_0=0, sum_1=0;
+        double sum2_0=0, sum2_1=0;
+        int n_0=0, n_1=0, n_na=0;
+        double x_tmp=0;
+
+        for(int i=0 ; i<N ; ++i){
+
+            x_tmp = mat_vars(i, k);
+
+            if(std::isnan(x_tmp) || std::isinf(x_tmp)){
+                ++n_na;
+            } else {
+                if(treat[i] == 0){
+                    sum_0 += x_tmp;
+                    sum2_0 += x_tmp*x_tmp;
+                    ++n_0;
+                } else {
+                    sum_1 += x_tmp;
+                    sum2_1 += x_tmp*x_tmp;
+                    ++n_1;
+                }
+            }
+        }
+
+        // saving
+        double m_0 = sum_0/n_0;
+        double m_1 = sum_1/n_1;
+        mean_mat(k, 0) = m_0;
+        mean_mat(k, 1) = m_1;
+
+        sd_mat(k, 0) = sqrt(sum2_0/(n_0 - 1) - m_0*sum_0/(n_0 - 1));
+        sd_mat(k, 1) = sqrt(sum2_1/(n_1 - 1) - m_1*sum_1/(n_1 - 1));
+
+        n_mat(k, 0) = n_0;
+        n_mat(k, 1) = n_1;
+
+        na_vect[k] = n_na;
+    }
+
+    // number of obs per treat case
+    for(int i=0 ; i<N ; ++i){
+        if(treat[i] == 0){
+            ++n_01[0];
+        } else {
+            ++n_01[1];
+        }
+    }
+
+    List res;
+    res["means"] = mean_mat;
+    res["sd"] = sd_mat;
+    res["n"] = n_mat;
+    res["n_01"] = n_01;
+    res["na"] = na_vect;
+
+    return res;
+}
 
 
 
