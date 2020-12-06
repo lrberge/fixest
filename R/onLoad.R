f6b7958ec040e48c3edc234b08c058e7da53fb19
lrberge <laurent.berge@hotmail.fr>
2020-12-06 22:53:38 +0100

    Update fixest_print

--- R/onLoad.R
+++ R/onLoad.R
@@ -6,7 +6,7 @@
 
 	options("fixest_dict" = c())
 	options("fixest_notes" = TRUE)
-	options("fixest_print.type" = "table")
+	options("fixest_print" = list(type = "table"))
 	options("fixest_fl_authorized" = FALSE)
 
 	setFixest_coefplot("all", reset = TRUE)
