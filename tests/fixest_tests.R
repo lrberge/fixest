89e87421022cd0523da54135cadd9413efe34256
lrberge <laurent.berge@hotmail.fr>
2021-04-02 11:32:40 +0200

    Add fitstat tests

--- tests/fixest_tests.R
+++ tests/fixest_tests.R
@@ -1440,10 +1440,24 @@ test(v3, v3b)
 test(max(abs(v1 - v3)) == 0, FALSE)
 test(max(abs(v2 - v3)) == 0, FALSE)
 
+####
+#### fitstat ####
+####
 
+base = iris
+names(base) = c("y", "x1", "x_endo_1", "x_inst_1", "fe")
+set.seed(2)
+base$x_inst_2 = 0.2 * base$y + 0.2 * base$x_endo_1 + rnorm(150, sd = 0.5)
+base$x_endo_2 = 0.2 * base$y - 0.2 * base$x_inst_1 + rnorm(150, sd = 0.5)
 
+# Checking a basic estimation
+est_iv = feols(y ~ x1 | x_endo_1 + x_endo_2 ~ x_inst_1 + x_inst_2, base)
 
+fitstat(est_iv, ~ f + ivf + ivf2 + wald + ivwald + ivwald2 + wh + sargan + rmse + g + n + ll + sq.cor + r2)
 
+est_fe = feols(y ~ x1 | fe, base)
+
+fitstat(est_fe, ~ wf)
 
 
 
