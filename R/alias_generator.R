a9cd761464875cf9ca041f1ededd9a8fde158d4c
lrberge <laurent.berge@hotmail.fr>
2021-08-03 00:01:15 +0200

    Safeguard against Rmarkdown

--- R/alias_generator.R
+++ R/alias_generator.R
@@ -7,7 +7,7 @@
 
 # The problem with aliased functions is that when the main-function changes its
 # arguments, then we also need to make the changes in the aliased ones, we usually need to
-# make those changes by hand, incurring substantial maintainance costs and highly susceptible to errors
+# make those changes by hand, incurring substantial maintenance costs and highly susceptible to errors
 #
 # It is much safer to generate aliased functions automatically
 # This cuts the maintenance costs to almost 0 and avoids any mistake
@@ -21,7 +21,8 @@ create_aliases = function(){
     # The function knows it's "me" with the system environment variable fixest_ROOT
 
     if(!isTRUE(renvir_get("fixest_ROOT"))) return(NULL)
-
+    # we check we're in the right directory (otherwise there can be prblms with Rmakdown)
+    if(!isTRUE(file.exists("R/VCOV_aliases.R"))) return(NULL)
 
     gen_etable_aliases()
     gen_iplot()
