16e966135ccfaebad75cb29c2fa937c933f6fe14
lrberge <laurent.berge@hotmail.fr>
2022-04-20 16:32:08 +0200

    create methods for coeftable/se/tstat/pvalue

--- R/MiscFuns.R
+++ R/MiscFuns.R
@@ -671,22 +671,204 @@ summary.fixest_list = function(object, se, cluster, ssc = getFixest_ssc(), .vcov
     res
 }
 
+
+#' Extracts the coefficients table from an estimation
+#'
+#' Methods to extracts the coefficients table and its sub-components from an estimation.
+#'
+#' @param object An estimation (fitted model object), e.g. a \code{fixest} object.
+#' @param ... Other arguments to the methods.
+#'
+#' @return
+#' Returns a matrix (\code{coeftable}) or vectors.
+#'
+#' @seealso
+#' Please look at the \code{\link[fixest]{coeftable.fixest}} page for more detailed information.
+#'
+#' @examples
+#'
+#' est = lm(mpg ~ cyl, mtcars)
+#' coeftable(est)
+#'
+coeftable = function(object, ...){
+    UseMethod("coeftable")
+}
+
+#' @describeIn coeftable Extracts the standard-errors from an estimation
+se = function(object, ...){
+    UseMethod("se")
+}
+
+#' @describeIn coeftable Extracts the p-values from an estimation
+pvalue = function(object, ...){
+    UseMethod("pvalue")
+}
+
+#' @describeIn coeftable Extracts the t-statistics from an estimation
+tstat = function(object, ...){
+    UseMethod("tstat")
+}
+
+
+#' Extracts the coefficients table from an estimation
+#'
+#' Default method to extracts the coefficients table and its sub-components from an estimation.
+#'
+#' @inheritParams etable
+#'
+#' @method coeftable default
+#'
+#' @param object The result of an estimation (a fitted model object). Note that this function is made to work with \code{fixest} objects so it may not work for the specific model you provide.
+#' @param ... Other arguments that will be passed to \code{summary}.
+#'
+#' First the method summary is applied if needed, then the coefficients table is extracted from its output.
+#'
+#' The default method is very naive and hopes that the resulting coefficients table contained in the summary of the fitted model is well formed: this assumption is very often wrong. Anyway, there is no development intended since the coeftable/se/pvalue/tstat series of methods is only intended to work well with \code{fixest} objects. To extract the coefficients table from fitted models in a general way, it's better to use \href{https://broom.tidymodels.org/}{tidy from broom}.
+#'
+#' @return
+#' Returns a matrix (\code{coeftable}) or vectors.
+#'
+#' @examples
+#'
+#' est = lm(mpg ~ cyl, mtcars)
+#' coeftable(est)
+#'
+#' se(est)
+#'
+#'
+coeftable.default = function(object, keep, drop, order, ...){
+    # This function is EXTREMELY naive and I don't intend to improve it
+    # => there is tidy for that which is much better
+    # I just created that method to handle fixest/fixest_multi more easily
+
+    check_arg(keep, drop, order, "NULL character vector no na")
+
+    if(!any(grepl("summary", class(object)))){
+
+        sum_exists = FALSE
+        for(c_name in class(object)){
+            if(exists(paste0("summary.", c_name), mode = "function")){
+                sum_exists = TRUE
+                break
+            }
+        }
+
+        if(sum_exists){
+            object = summary(object, ...)
+        }
+    }
+
+    list_mat = object[sapply(object, is.matrix)]
+
+    ok = FALSE
+    for(i in seq_along(list_mat)){
+        mat = list_mat[[i]]
+        if(!is.null(colnames(mat)) && any(grepl("(?i)(coef|estimate|value|Pr\\()", colnames(mat)))){
+            ok = TRUE
+            res = mat
+        }
+    }
+
+    if(ok == FALSE){
+        stop("Sorry, the coeffficients table could not be extracted.")
+    }
+
+    if(!missnull(keep) || !missnull(drop) || !missnull(order)){
+        r_names = rownames(res)
+        r_names = keep_apply(r_names, keep)
+        r_names = drop_apply(r_names, drop)
+        r_names = order_apply(r_names, order)
+
+        if(length(r_names) == 0){
+            return(NULL)
+        }
+
+        res = res[r_names, , drop = FALSE]
+    }
+
+    res
+}
+
+
+#' @describeIn coeftable.default Extracts the standard-errors from an estimation
+se.default = function(object, keep, drop, order, ...){
+    # There is NO GARANTEE that it works
+
+    check_arg(keep, drop, order, "NULL character vector no na")
+
+    mat = coeftable(object, keep = keep, drop = drop, order = order, ...)
+
+    if(is.null(mat)){
+        return(NULL)
+    }
+
+    res = mat[, 2]
+    res
+}
+
+#' @describeIn coeftable.default Extracts the standard-errors from an estimation
+tstat.default = function(object, keep, drop, order, ...){
+    # There is NO GARANTEE that it works
+
+    check_arg(keep, drop, order, "NULL character vector no na")
+
+    mat = coeftable(object, keep = keep, drop = drop, order = order, ...)
+
+    if(is.null(mat)){
+        return(NULL)
+    }
+
+    if(ncol(mat) != 4){
+        stop("The p-values could not be obtained. Use broom::tidy?")
+    }
+
+    res = mat[, 3]
+    res
+}
+
+#' @describeIn coeftable.default Extracts the p-values from an estimation
+pvalue.default = function(object, keep, drop, order, ...){
+    # There is NO GARANTEE that it works
+
+    check_arg(keep, drop, order, "NULL character vector no na")
+
+    mat = coeftable(object, keep = keep, drop = drop, order = order, ...)
+
+    if(is.null(mat)){
+        return(NULL)
+    }
+
+    if(ncol(mat) != 4){
+        stop("The p-values could not be obtained. Use broom::tidy?")
+    }
+
+    res = mat[, 4]
+    res
+}
+
+
+
+
 #' Obtain various statistics from an estimation
 #'
 #' Set of functions to directly extract some commonly used statistics, like the p-value or the table of coefficients, from estimations. This was first implemented for \code{fixest} estimations, but has some support for other models.
 #'
 #' @inheritParams etable
 #'
-#' @param object An estimation. For example obtained from \code{\link[fixest]{feols}}.
-#' @param se [Fixest specific.] Character scalar. Which kind of standard error should be computed: \dQuote{iid}, \dQuote{hetero}, \dQuote{cluster}, \dQuote{twoway}, \dQuote{threeway} or \dQuote{fourway}? By default if there are fixed-effects in the estimation: \code{se = "cluster"}, otherwise \code{se = "iid"}. Note that this argument is not needed if the argument \code{cluster} is present.
+#' @method coeftable fixest
+#'
+#' @param object A \code{fixest} object. For example an estimation obtained from \code{\link[fixest]{feols}}.
 #' @param cluster [Fixest specific.] Tells how to cluster the standard-errors (if clustering is requested). Can be either a list of vectors, a character vector of variable names, a formula or an integer vector. Assume we want to perform 2-way clustering over \code{var1} and \code{var2} contained in the data.frame \code{base} used for the estimation. All the following \code{cluster} arguments are valid and do the same thing: \code{cluster = base[, c("var1, "var2")]}, \code{cluster = c("var1, "var2")}, \code{cluster = ~var1+var2}. If the two variables were used as clusters in the estimation, you could further use \code{cluster = 1:2} or leave it blank with \code{se = "twoway"} (assuming \code{var1} [resp. \code{var2}] was the 1st [res. 2nd] cluster).
-#' @param ... Other arguments to be passed to \code{summary}.
+#' @param list Logical, default is \code{FALSE}. If \code{TRUE}, then a nested list is returned, the first layer is accessed with the coefficients names; the second layer with the following values: \code{coef}, \code{se}, \code{tstat}, \code{pvalue}. Note that the variable \code{"(Intercept)"} is renamed into \code{"constant"}.
+#' @param ... Other arguments to be passed to \code{\link[fixest]{summary.fixest}}.
 #'
 #' @details
-#' This set of functions is primarily constructed for \code{fixest} estimations. Although it can work for non-\code{fixest} estimations, support for exotic estimation procedures that do not report standardized coefficient tables is highly limited.
+#' This set of tiny functions is primarily constructed for \code{fixest} estimations.
 #'
 #' @return
 #' Returns a table of coefficients, with in rows the variables and four columns: the estimate, the standard-error, the t-statistic and the p-value.
+#'
+#' If \code{list = TRUE} then a nested list is returned, the first layer is accessed with the coefficients names; the second layer with the following values: \code{coef}, \code{se}, \code{tstat}, \code{pvalue}. For example, with \code{res = coeftable(est, list = TRUE)} you can access the SE of the coefficient \code{x1} with \code{res$x1$se}; and its coefficient with \code{res$x1$coef}, etc.
 #'
 #' @examples
 #'
@@ -732,56 +914,39 @@ summary.fixest_list = function(object, se, cluster, ssc = getFixest_ssc(), .vcov
 #'
 #' coeftable(est_iv, order = "!Int")
 #'
+#' #
+#' # Using lists
+#' #
+#'
+#' # Returning the coefficients table as a list can be useful for quick
+#' # reference in markdown documents.
+#' # Note that the "(Intercept)" is renamed into "constant"
+#'
+#' res = coeftable(est_iv, list = TRUE)
+#'
+#' # coefficient of the constant:
+#' res$constant$coef
+#'
+#' # pvalue of x1
+#' res$x1$pvalue
 #'
 #'
-coeftable = function(object, vcov = NULL, ssc = NULL, cluster = NULL, keep, drop, order, ...){
+#'
+coeftable.fixest = function(object, vcov = NULL, ssc = NULL, cluster = NULL,
+                            keep = NULL, drop = NULL, order = NULL, list = FALSE, ...){
     # We don't explicitly refer to the other arguments
 
     check_arg(keep, drop, order, "NULL character vector no na")
+    check_arg(list, "logical scalar")
 
-    IS_FIXEST = "fixest" %in% class(object)
-    # IS_FIXEST_MULI = "fixest_multi" %in% class(object)
-    # IS_FIXEST_LIST = "fixest_multi" %in% class(object)
 
-    if(IS_FIXEST){
-        if(!isTRUE(object$summary) || !(all_missing(vcov, ssc, cluster) && ...length() > 0)){
-            object = summary(object, vcov = vcov, ssc = ssc, cluster = cluster, ...)
-        }
-    } else {
-        if(!any(grepl("summary", class(object)))){
-            # Known issue:
-            # - if vcov/.../order are also args of summary, that will be a problem
-
-            args = list(object = object)
-
-            if(...length() > 0){
-                args = append(args, list(...))
-            }
-
-            object = do.call("summary", args)
-        }
+    if(!isTRUE(object$summary) || !(all_missing(vcov, ssc, cluster) && ...length() > 0)){
+        object = summary(object, vcov = vcov, ssc = ssc, cluster = cluster, ...)
     }
 
     # Let's find out the coefficients table
-    if(IS_FIXEST){
-        res = object$coeftable
-    } else {
-        list_mat = object[sapply(object, is.matrix)]
-
-        ok = FALSE
-        for(i in seq_along(list_mat)){
-            mat = list_mat[[i]]
-            if(!is.null(colnames(mat)) && any(grepl("(?i)(estimate|value|Pr\\()", colnames(mat)))){
-                ok = TRUE
-                res = mat
-            }
-        }
-
-        if(ok == FALSE){
-            stop("Sorry, the coeffficients table could not be extracted.")
-        }
-
-    }
+    res = object$coeftable
+    attr(res, "type") = NULL
 
     if(!missnull(keep) || !missnull(drop) || !missnull(order)){
         r_names = rownames(res)
@@ -796,109 +961,69 @@ coeftable = function(object, vcov = NULL, ssc = NULL, cluster = NULL, keep, drop
         res = res[r_names, , drop = FALSE]
     }
 
+    if(list){
+        res_list = list()
+        coef_names = rownames(res)
+        coef_names[coef_names == "(Intercept)"] = "constant"
+        for(i in 1:nrow(res)){
+            row = res[i, ]
+            item = list(coef = row[1], se = row[2], tstat = row[3], pvalue = row[4])
+            res_list[[coef_names[i]]] = item
+        }
+        res = res_list
+    }
+
     res
 }
 
 #' @describeIn coeftable Extracts the p-value of an estimation
-pvalue = function(object, vcov = NULL, ssc = NULL, cluster = NULL, keep, drop, order, ...){
+pvalue.fixest = function(object, vcov = NULL, ssc = NULL, cluster = NULL,
+                         keep = NULL, drop = NULL, order = NULL, ...){
 
     check_arg(keep, drop, order, "NULL character vector no na")
 
-    mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster, ...)
+    mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster,
+                    keep = keep, drop = drop, order = order, ...)
 
-    if(ncol(mat) != 4){
-        stop("No appropriate coefficient table found (number of columns is ", ncol(mat), " instead of 4), sorry.")
+    if(is.null(mat)){
+        return(NULL)
     }
 
     res = mat[, 4]
-    if(is.null(names(res))) {
-        names(res) = rownames(mat)
-    }
-
-    if(!missnull(keep) || !missnull(drop) || !missnull(order)){
-        r_names = names(res)
-        r_names = keep_apply(r_names, keep)
-        r_names = drop_apply(r_names, drop)
-        r_names = order_apply(r_names, order)
-
-        if(length(r_names) == 0){
-            return(numeric(0))
-        }
-
-        res = res[r_names]
-    }
-
     res
 }
 
 #' @describeIn coeftable Extracts the t-statistics of an estimation
-tstat = function(object, vcov = NULL, ssc = NULL, cluster = NULL, keep, drop, order, ...){
+tstat.fixest = function(object, vcov = NULL, ssc = NULL, cluster = NULL,
+                        keep = NULL, drop = NULL, order = NULL, ...){
 
     check_arg(keep, drop, order, "NULL character vector no na")
 
-    mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster, ...)
+    mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster,
+                    keep = keep, drop = drop, order = order, ...)
 
-    if(ncol(mat) != 4){
-        stop("No appropriate coefficient table found (number of columns is ", ncol(mat), " instead of 4), sorry.")
+    if(is.null(mat)){
+        return(NULL)
     }
 
     res = mat[, 3]
-    if(is.null(names(res))) {
-        names(res) = rownames(mat)
-    }
-
-    if(!missnull(keep) || !missnull(drop) || !missnull(order)){
-        r_names = names(res)
-        r_names = keep_apply(r_names, keep)
-        r_names = drop_apply(r_names, drop)
-        r_names = order_apply(r_names, order)
-
-        if(length(r_names) == 0){
-            return(numeric(0))
-        }
-
-        res = res[r_names]
-    }
-
     res
 }
 
 #' @describeIn coeftable Extracts the standard-error of an estimation
-se = function(object, vcov = NULL, ssc = NULL, cluster = NULL, keep, drop, order, ...){
+se.fixest = function(object, vcov = NULL, ssc = NULL, cluster = NULL,
+                     keep = NULL, drop = NULL, order = NULL, ...){
 
     check_arg(keep, drop, order, "NULL character vector no na")
 
-    if(is.matrix(object) && nrow(object) == ncol(object)){
-        # special case => object is a VCOV matrix and NOT an estimation
-        res = sqrt(diag(object))
+    mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster,
+                    keep = keep, drop = drop, order = order, ...)
 
-    } else {
-
-        mat = coeftable(object, vcov = vcov, ssc = ssc, cluster = cluster, ...)
-
-        if(ncol(mat) != 4){
-            stop("No appropriate coefficient table found (number of columns is ", ncol(mat), " instead of 4), sorry.")
-        }
-
-        res = mat[, 2]
-        if(is.null(names(res))) {
-            names(res) = rownames(mat)
-        }
+    if(is.null(mat)){
+        return(NULL)
     }
 
-    if(!missnull(keep) || !missnull(drop) || !missnull(order)){
-        r_names = names(res)
-        r_names = keep_apply(r_names, keep)
-        r_names = drop_apply(r_names, drop)
-        r_names = order_apply(r_names, order)
-
-        if(length(r_names) == 0){
-            return(numeric(0))
-        }
-
-        res = res[r_names]
-    }
-
+    res = mat[, 2]
     res
 }
 
