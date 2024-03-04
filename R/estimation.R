0a3b8f890c43f947bd9993c1c4d5c56064d2927d
lrberge <laurent.berge@hotmail.fr>
2024-03-04 11:33:09 +0100

    update doc: fixef.rm fixes: #478

--- R/estimation.R
+++ R/estimation.R
@@ -3696,7 +3696,8 @@ fepois = function(fml, data, vcov, offset, weights, subset, split, fsplit,
 #' Controls which observations are to be removed. If "perfect", then observations 
 #' having a fixed-effect with perfect fit (e.g. only 0 outcomes in Poisson estimations) 
 #' will be removed. If "singleton", all observations for which a fixed-effect appears 
-#' only once will be removed. The meaning of "both" and "none" is direct.
+#' only once will be removed. Note, importantly, that singletons are removed in just one pass, 
+#' there is no recursivity implemented. The meaning of "both" and "none" is direct.
 #' @param fixef.tol Precision used to obtain the fixed-effects. Defaults to `1e-5`. 
 #' It corresponds to the maximum absolute difference allowed between two coefficients 
 #' of successive iterations. Argument `fixef.tol` cannot be lower 
